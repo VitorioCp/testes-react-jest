ff70e438ccc3b5dc3cc2f66713f0e965fa19b3c7
Vitório Corrêa <vitcorreapereira@hotmail.com>
2024-11-23 21:56:44 -0300

    first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const sum = (x: number, y: number) => {
+  return x + y;
+};
+
+describe("App Component", () => {
+  it("should sum correctly", () => {
+    expect(sum(4, 4)).toBe(8);
+  });
+
+  it("should render App with hello message", () => {
+    render(<App />);
+
+    screen.findByText("Olá mundo");
+  });
+
+  it("mude o nome", () => {
+    render(<App />);
+
+    screen.findByText("Vitorio");
+    const btn = screen.getByText(/mudar msg/i);
+    fireEvent.click(btn);
+
+    screen.getByText(/Correa/i);
+
+    screen.queryByText("Vitorio")
+  });
+
+
 });
+
+export default {};
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Tasks from './components/Tasks';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Tasks />
   </React.StrictMode>
 );
 
